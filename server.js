191551dcbf56bdedbcf43fe74fa4511513ce333e
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2023-11-22 19:58:29 -0500

    edited server.js

--- server.js
+++ server.js
@@ -1,46 +1,94 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const fs = require ('fs/promises');
+const fs = require('fs').promises;
+const { v4: uuidv4 } = require('uuid');
 
+const app = express();
+const PORT = process.env.PORT || 3001
+const dbFilePath = path.join(__dirname, 'Develop', 'db', 'db.json');
 
-//base URL
-const basePath = process.env.NODE_ENV === 'production' ? '/miniature-eureka' : '';
-//middleware
-app.use(basePath, express.static(path.join(__dirname, 'Develop', 'public')));
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'Develop', 'public')));
+
+//checking if db exists and creating if not
+fs.access(dbFilePath)
+  .catch(() => fs.writeFile(dbFilePath, '[]'))
+  .then(() => {
+    console.log('Database file ready.');
+  })
+  .catch((err) => {
+    console.error('Error creating database file:', err);
+  });
 
 
 //route for Notes
-app.get(basePath + '/notes', (req, res) => {
-    res.sendFile('notes.html', { root: path.join(__dirname, 'Develop', 'public') });
-});
+app.get('/notes', (req, res) => {
+    console.log('testing getting notes');
+    res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
+  });
+
+// Handle GET request to retrieve notes
+app.get('/api/notes', (req, res) => {
+    console.log('testing retrieving notes');
+    fs.readFile(dbFilePath, 'utf8')
+      .then((data) => {
+        const notes = JSON.parse(data);
+        res.json(notes);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
 //save notes
-app.post('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-});
+app.post('/api/notes', express.json(), async (req, res) => {
+    try {
+      const { title, text } = req.body;
+  
+      if (!title || !text) {
+        return res.status(400).json({ error: 'Title and text are required' });
+      }
+  
+      const data = await fs.readFile(dbFilePath, 'utf8');
+      const notes = JSON.parse(data);
+      const newNote = { id: uuidv4(), title, text };
+      notes.push(newNote);
+  
+      await fs.writeFile(dbFilePath, JSON.stringify(notes, null, 2));
+      res.status(201).json(newNote);
+      console.log('testing saving notes');
+    } catch (err) {
+      console.error('Error saving note:', err);
+      res.status(500).json({ error: 'Failed to save note' });
+    }
+  });
 
 //delete notes
-app.delete('/api/notes:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
-    fs.readFile('./db.json', 'utf8')
-    .then((data) => {
+    fs.readFile(dbFilePath, 'utf8')
+      .then((data) => {
         const notes = JSON.parse(data);
         const updatedNotes = notes.filter((note) => note.id !== noteId);
-
-        return fs.writeFile('./db.json', JSON. stringify(updatedNotes, null, 2));
-    })
-    .then(() => {
-        res.json(`Item ${noteId} has been deleted 🗑️`)
-    })
-    .catch((err) => {
+  
+        return fs.writeFile(dbFilePath, JSON.stringify(updatedNotes, null, 2));
+      })
+      .then(() => {
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+        console.log('testing deleting notes');
+      })
+      .catch((err) => {
         res.status(500).json({ error: err.message });
-    });
-});
+      });
+  });
 
 
 //start server
-const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
+
+
+//const generateUniqueId = () => {
+  //  return uuidv4(); // Generate a version 4 (random) UUID
+  //};
